c09508b6d3247ba7834715412d0e5401fbb3a777
Tapish Khandelwal <tapishkhandelwal13@gmail.com>
2024-01-25 11:55:57 +0530

    add audio download mechanism

--- app.rb
+++ app.rb
@@ -1,15 +1,22 @@
-
+# ADD necessary requirements
 require 'net/http'
 require 'json'
 require 'bundler/inline'
+require 'open-uri'
+require 'fileutils'
 
+# Install additional gems
 gemfile do
   source 'https://rubygems.org'
   gem 'dotenv', groups: [:development, :test]
+  # gem "progressbar"
 end
 
 require 'dotenv/load'
+# require 'progressbar'
 
+# Fireflies.ai supports GraphQL, thus the required grapghql query to fetch
+# data via POST API call
 query = """
 query Transcripts(
         $limit: Int
@@ -76,13 +83,14 @@ query Transcripts(
       }
 """
 
+# Base request parameters
 base_url = "https://api.fireflies.ai"
 path = "/graphql"
 endpoint = "#{base_url}#{path}"
-
 parsed_uri = URI.parse(endpoint)
-
 authorization_token = "Bearer #{ENV['FIREFLIES_API_KEY']}"
+
+# Post request definition
 req = Net::HTTP::Post.new(
   parsed_uri.path,
   {
@@ -99,7 +107,7 @@ req.body = {
   }
 }.to_json
 
-
+# Persistent connection to fetch data
 res = Net::HTTP.start(
   parsed_uri.host,
   parsed_uri.port,
@@ -108,4 +116,83 @@ res = Net::HTTP.start(
     res = http.request(req)
 end
 
-puts res.inspect
+puts "Response code is #{res.code}"
+
+# Root directory for downloads
+base_path = "."
+
+# Execute when a valid response is fetched
+if res.code == "200"
+  data = JSON.parse(res.body).dig("data","transcripts")
+  # Execute when transcripts data is available
+  unless data.empty?
+    data.each do |transcript|
+      # Parsed fields from response
+      audio_url = transcript["audio_url"]
+      video_url = transcript["video_url"]
+      meeting_timestamp = transcript["date"]
+      file_title = transcript["title"]
+
+      # Directory and filename based on response
+      file_timestamp = "#{
+        Time.at(meeting_timestamp/1000).strftime('%Y-%m-%d/%H:%M:%S.%3N')
+        }" if meeting_timestamp
+      
+      directory = if file_timestamp.empty?
+        "#{base_path}/downloads"
+      else
+        "#{base_path}/#{file_timestamp}"
+      end
+
+      file_name = "#{directory}/#{file_title}"
+
+      # Create directory if doesn't exist
+      FileUtils.mkdir_p(directory) unless File.directory?(directory)
+
+      # Download the audio file
+      if audio_url
+        start_time = Time.now
+        URI.open(audio_url, "Accept-Encoding" => "gzip, deflate, br") do |content|
+          File.open("#{file_name}.mp3", 'wb') do |file|
+            file << content.read
+          end
+        end
+        puts "#{Time.now - start_time}"
+      end
+
+      # Download the video file
+      # if video_url
+      #   start_time = Time.now
+      #   URI.open(video_url, "Accept-Encoding" => "gzip, deflate, br") do |content|
+      #     File.open("#{file_name}.mp4", 'wb') do |file|
+      #       file << content.read
+      #     end
+      #   end
+      # end
+
+      ## Experiment with Net/Http file download 
+      # start_time = Time.now
+      # begin
+      #   parsed_uri = URI.parse(video_url)
+      #   Net::HTTP.start(
+      #     parsed_uri.host,
+      #     parsed_uri.port,
+      #     use_ssl: true
+      #   ) do |http|
+      #     request = Net::HTTP::Get.new parsed_uri
+      #     request["Accept-Encoding"] = "gzip, deflate, br"
+      #     http.request(request) do |response|
+      #       File.open("video6.mp4", "wb") do |file|
+      #         response.read_body do |chunk|
+      #           file.write chunk
+      #         end
+      #       end
+      #     end
+      #   end    
+      # rescue StandardError => e
+      #   puts e.inspect
+      # end
+      # puts "#{Time.now - start_time}"
+    end
+  end
+end
